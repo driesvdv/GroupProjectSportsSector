879e8487964b104f7b608ca609c8add7ab9e2a62
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-03-25 23:10:39 +0100

    Registration controller routes

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,9 +3,69 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Registrant;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegistrationController extends Controller
 {
-    //
+    public function index($registrant_id){
+        try {
+            $user = Auth::user();
+            $registrant = Registrant::where([
+                ['id', '=', $registrant_id],
+                ['user_id', '=', $user->id]
+            ])->first();
+
+            return response()->json([$registrant], 200);
+        }catch (Exception $e){
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+    }
+
+    public function store(Request $request, $registrant_id){
+        try {
+            $user = Auth::user();
+            $registrant = new Registrant();
+
+            $data = request()->validate([
+                'group_id' => 'required',
+                'registrant_id' => 'required',
+                'has_paid' => 'required',
+            ]);
+
+            $registrant->group_id = $data['group_id'];
+            $registrant->registrant_id = $data['registrant_id'];
+            $registrant->has_paid = $data['has_paid'];
+
+            return response()->json([$registrant], 200);
+        }catch (Exception $e){
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+    }
+
+    public function update($registrant_id){
+        try {
+            $user = Auth::user();
+            $registrant = Registrant::where([
+                ['id', '=', $registrant_id],
+                ['user_id', '=', $user->id]
+            ])->first();
+
+            $data = request()->validate([
+                'group_id' => 'required',
+                'registrant_id' => 'required',
+                'has_paid' => 'required',
+            ]);
+
+            $registrant->group_id = $data['group_id'];
+            $registrant->registrant_id = $data['registrant_id'];
+            $registrant->has_paid = $data['has_paid'];
+
+            return response()->json([$registrant], 200);
+        }catch (Exception $e){
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+    }
 }
