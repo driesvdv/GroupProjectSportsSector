b850f12db8eb79d1f1a499bcb4e2585f89efcc8c
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-03-26 10:09:49 +0100

    groups actions

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,9 +3,72 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Group;
+use App\Models\Registrant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GroupController extends Controller
 {
-    //
+    public function index($group_name){
+        try {
+            $group = Registrant::where([
+                ['name', '=', $group_name]
+            ])->first();
+
+            return response()->json([$group], 200);
+        }catch (Exception $e){
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+    }
+
+    public function store(Request $request){
+        try {
+            $group = new Group();
+
+            $data = request()->validate([
+                'name' => 'required',
+                'time' => 'required',
+                'max_members' => 'required',
+                'sportclub_id' => 'required'
+            ]);
+
+            $group->name = $data['name'];
+            $group->time = $data['time'];
+            $group->max_members = $data['max_members'];
+            $group->sportclub_id = $data['sportclub_id'];
+
+            $group->save();
+
+            return response()->json([$group], 200);
+        }catch (Exception $e){
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+    }
+
+    public function update(Request $request, $group_name){
+        try {
+            $group = Registrant::where([
+                ['name', '=', $group_name]
+            ])->first();
+
+            $data = request()->validate([
+                'name' => 'required',
+                'time' => 'required',
+                'max_members' => 'required',
+                'sportclub_id' => 'required'
+            ]);
+
+            $group->name = $data['name'];
+            $group->time = $data['time'];
+            $group->max_members = $data['max_members'];
+            $group->sportclub_id = $data['sportclub_id'];
+
+            $group->save();
+
+            return response()->json([$group], 200);
+        }catch (Exception $e){
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+    }
 }
