09b76464c45efe1a1ecdcf444a69507950fe6270
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-03-26 10:10:08 +0100

    new routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\RegistrantController;
+use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\SportclubController;
+use App\Http\Controllers\SportSessionController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 
@@ -26,5 +30,24 @@ Route::put('/{sportclub_name}', [SportclubController::class, 'update']);
 //user
 Route::get('/user', [UserController::class, 'index']);
 Route::post('/user', [UserController::class, 'store']);
-Route::patch('/user', [UserController::class, 'update']);
+Route::put('/user', [UserController::class, 'update']);
 
+//registrants
+Route::get('/registrants/{registrant_id}', [RegistrantController::class, 'index']);
+Route::post('/registrants', [RegistrantController::class, 'store']);
+Route::put('/registrants/{registrant_id}', [RegistrantController::class, 'update']);
+
+//registrations
+Route::get('/registrants/{registrant_id}/registrations/{registration_id}', [RegistrationController::class, 'index']);
+Route::post('/registrants/{registrant_id}/registrations', [RegistrationController::class, 'store']);
+Route::put('/registrants/{registrant_id}/registrations', [RegistrationController::class, 'update']);
+
+//groups
+Route::get('/groups/{group_name}', [GroupController::class, 'index']);
+Route::post('/groups', [GroupController::class, 'store']);
+Route::put('/groups/{group_name}', [GroupController::class, 'update']);
+
+//sportsessions
+Route::get('/sportsession/{session_id}', [SportSessionController::class, 'index']);
+Route::post('/sportsessions', [SportSessionController::class, 'store']);
+Route::put('/sportsessions/{session_id}', [SportSessionController::class, 'update']);
