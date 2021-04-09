1741668e6c3435829ac7b3139848b51d6d1e1dd8
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-04-09 15:50:41 +0200

    Group resource

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,81 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\GroupResource;
 use App\Models\Group;
 use App\Models\Registrant;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
 
 class GroupController extends Controller
 {
     public function show($group_name){
-        try {
-            $group = Group::where([
-                ['name', '=', $group_name]
-            ])->first();
+        $group = Group::where([
+            ['name', '=', $group_name]
+        ])->firstOrFail();
 
-            return response()->json([$group], 200);
-        }catch (Exception $e){
-            return response()->json(['message' => $e->getMessage()], 400);
-        }
+        return new GroupResource($group);
     }
 
     public function index(){
-        try {
-            $groups = Group::all();
-
-            return response()->json([$groups], 200);
-        }catch (Exception $e){
-            return response()->json(['message' => $e->getMessage()], 400);
-        }
+        return GroupResource::collection(Group::all());
     }
     public function store(Request $request){
-        try {
-            $group = new Group();
+        $group = $this->ValidateGroup(new Group(), $request);
 
-            $data = request()->validate([
-                'name' => 'required',
-                'time' => 'required',
-                'max_members' => 'required',
-                'sportclub_id' => 'required'
-            ]);
-
-            $group->name = $data['name'];
-            $group->time = $data['time'];
-            $group->max_members = $data['max_members'];
-            $group->sportclub_id = $data['sportclub_id'];
-
-            $group->save();
-
-            return response()->json([$group], 200);
-        }catch (Exception $e){
-            return response()->json(['message' => $e->getMessage()], 400);
-        }
+        return new GroupResource($group);
     }
 
     public function update(Request $request, $group_name){
-        try {
-            $group = Group::where([
-                ['name', '=', $group_name]
-            ])->first();
+        $group = Group::where([
+            ['name', '=', $group_name]
+        ])->firstOrFail();
 
-            $data = request()->validate([
-                'name' => 'required',
-                'time' => 'required',
-                'max_members' => 'required',
-                'sportclub_id' => 'required'
-            ]);
+        $group = $this->ValidateGroup($group, $request);
 
-            $group->name = $data['name'];
-            $group->time = $data['time'];
-            $group->max_members = $data['max_members'];
-            $group->sportclub_id = $data['sportclub_id'];
+        return new GroupResource($group);
+    }
 
-            $group->save();
+    private function ValidateGroup(Group $group, Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'time' => 'required',
+            'max_members' => 'required',
+            'sportclub_id' => 'required'
+        ]);
 
-            return response()->json([$group], 200);
-        }catch (Exception $e){
-            return response()->json(['message' => $e->getMessage()], 400);
-        }
+        $group->name = $data['name'];
+        $group->time = $data['time'];
+        $group->max_members = $data['max_members'];
+        $group->sportclub_id = $data['sportclub_id'];
+
+        $group->save();
+
+        return $group;
     }
 }
