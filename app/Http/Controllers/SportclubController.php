37076e88a23a327632575a03f0a661d4c691be35
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-04-09 16:02:38 +0200

    Sportclub Resource

--- app/Http/Controllers/SportclubController.php
+++ app/Http/Controllers/SportclubController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SportclubResource;
 use App\Models\Sportclub;
 use Exception;
 use Illuminate\Http\Request;
@@ -11,48 +12,44 @@ use Illuminate\Support\Facades\Auth;
 class SportclubController extends Controller
 {
     public function show($sportclub_name){
-        try {
-            return Sportclub::where('name', $sportclub_name)->first();
-        }catch (Exception $exception){
-            return response()->json(['message' => $exception->getMessage()], 400);
-        }
+        $sportclub = Sportclub::where('name', $sportclub_name)->firstOrFail();
+
+        return new SportclubResource($sportclub);
     }
 
     public function index(){
-        try {
-            return Sportclub::all();
-        }catch (Exception $exception){
-            return response()->json(['message' => $exception->getMessage()], 400);
-        }
+        return SportclubResource::collection(Sportclub::all());
     }
 
     public function update(Request $request, $sportclub_name){
-        try {
-            $user = Auth::user();
-            $sportclub = Sportclub::where('name', $sportclub_name)->first();
+        $sportclub = Sportclub::where('name', $sportclub_name)->firstOrFail();
 
-            if (!$this->isAdmin($sportclub, $user)){
-                return response()->json(['message' => 'Permission denied'], 403);
-            }
-
-            $data = request()->validate([
-                'name' => 'required',
-                'sport_id' => 'required'
-            ]);
-
-            $sportclub->name = $data['name'];
-            $sportclub->sport_id = $data['sport_id'];
-
-            $sportclub->save();
-            return response()->json($sportclub, 403);
-
-        }catch (Exception $exception){
-            return response()->json(['message' => $exception->getMessage()], 400);
+        if (!$this->isAdmin($sportclub)){
+            return response()->json(['message' => 'Permission denied'], 403);
         }
+
+        $sportclub = $this->ValidateSportclub($request, $sportclub);
+        return new SportclubResource($sportclub);
     }
 
     //helpers
-    private function isAdmin($sportclub, $user){
+    private function isAdmin($sportclub){
         return true;
+    }
+
+
+    private function ValidateSportclub(Request $request, Sportclub $sportclub)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'sport_id' => 'required'
+        ]);
+
+        $sportclub->name = $data['name'];
+        $sportclub->sport_id = $data['sport_id'];
+
+        $sportclub->save();
+
+        return $sportclub;
     }
 }
