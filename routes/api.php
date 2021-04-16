5817b2ed4e67528ac2ef8b91c1e0c37c34c7b1b6
Dries Van de Velde <73098090+driesvdv@users.noreply.github.com>
2021-04-16 18:32:40 +0200

    :bug: fixed auth middleware

--- routes/api.php
+++ routes/api.php
@@ -36,34 +36,34 @@ Route::put('/{sportclub_name}', [SportclubController::class, 'update']);
 
 //user
 Route::get('/user', [UserController::class, 'show']);
-Route::post('/user', [UserController::class, 'store'])->middleware('auth');
-Route::put('/user', [UserController::class, 'update'])->middleware('auth');
+Route::post('/user', [UserController::class, 'store'])->middleware('auth:api');
+Route::put('/user', [UserController::class, 'update'])->middleware('auth:api');
 
 //registrants
-Route::get('/registrants/{registrant_id}', [RegistrantController::class, 'show'])->middleware('auth');
-Route::get('/registrants', [RegistrantController::class, 'index'])->middleware('auth');
-Route::post('/registrants', [RegistrantController::class, 'store'])->middleware('auth');
-Route::put('/registrants/{registrant_id}', [RegistrantController::class, 'update'])->middleware('auth');
+Route::get('/registrants/{registrant_id}', [RegistrantController::class, 'show'])->middleware('auth:api');
+Route::get('/registrants', [RegistrantController::class, 'index'])->middleware('auth:api');
+Route::post('/registrants', [RegistrantController::class, 'store'])->middleware('auth:api');
+Route::put('/registrants/{registrant_id}', [RegistrantController::class, 'update'])->middleware('auth:api');
 
 //registrations
-Route::get('/registrants/{registrant_id}/registrations/{registration_id}', [RegistrationController::class, 'show'])->middleware('auth');
-Route::get('/registrants/{registrant_id}/registrations', [RegistrationController::class, 'index'])->middleware('auth');
-Route::post('/registrants/{registrant_id}/registrations', [RegistrationController::class, 'store'])->middleware('auth');
-Route::put('/registrants/{registrant_id}/registrations/{registration_id}', [RegistrationController::class, 'update'])->middleware('auth');
+Route::get('/registrants/{registrant_id}/registrations/{registration_id}', [RegistrationController::class, 'show'])->middleware('auth:api');
+Route::get('/registrants/{registrant_id}/registrations', [RegistrationController::class, 'index'])->middleware('auth:api');
+Route::post('/registrants/{registrant_id}/registrations', [RegistrationController::class, 'store'])->middleware('auth:api');
+Route::put('/registrants/{registrant_id}/registrations/{registration_id}', [RegistrationController::class, 'update'])->middleware('auth:api');
 
 //groups
-Route::get('/groups/{group_name}', [GroupController::class, 'show'])->middleware('auth');
-Route::get('/groups', [GroupController::class, 'index'])->middleware('auth');
-Route::post('/groups', [GroupController::class, 'store'])->middleware('auth');
-Route::put('/groups/{group_name}', [GroupController::class, 'update'])->middleware('auth');
+Route::get('/groups/{group_name}', [GroupController::class, 'show'])->middleware('auth:api');
+Route::get('/groups', [GroupController::class, 'index'])->middleware('auth:api');
+Route::post('/groups', [GroupController::class, 'store'])->middleware('auth:api');
+Route::put('/groups/{group_name}', [GroupController::class, 'update'])->middleware('auth:api');
 
 //sportsessions
-Route::get('/sportsession/{session_id}', [SportSessionController::class, 'show'])->middleware('auth');
-Route::post('/sportsessions', [SportSessionController::class, 'store'])->middleware('auth');
-Route::put('/sportsessions/{session_id}', [SportSessionController::class, 'update'])->middleware('auth');
+Route::get('/sportsession/{session_id}', [SportSessionController::class, 'show'])->middleware('auth:api');
+Route::post('/sportsessions', [SportSessionController::class, 'store'])->middleware('auth:api');
+Route::put('/sportsessions/{session_id}', [SportSessionController::class, 'update'])->middleware('auth:api');
 
 //absentsessions
-Route::get('/sportsessions/{session_id}/absentsessions', [AbsentSessionController::class, 'index'])->middleware('auth');
-Route::get('/sportsessions/{session_id}/absentsessions/{absent_id}', [AbsentSessionController::class, 'show'])->middleware('auth');
-Route::post('/sportsessions/{session_id}/absentsessions', [AbsentSessionController::class, 'store'])->middleware('auth');
-Route::put('/sportsessions/{session_id}/absentsessions/{absent_id}', [AbsentSessionController::class, 'update'])->middleware('auth');
+Route::get('/sportsessions/{session_id}/absentsessions', [AbsentSessionController::class, 'index'])->middleware('auth:api');
+Route::get('/sportsessions/{session_id}/absentsessions/{absent_id}', [AbsentSessionController::class, 'show'])->middleware('auth:api');
+Route::post('/sportsessions/{session_id}/absentsessions', [AbsentSessionController::class, 'store'])->middleware('auth:api');
+Route::put('/sportsessions/{session_id}/absentsessions/{absent_id}', [AbsentSessionController::class, 'update'])->middleware('auth:api');
