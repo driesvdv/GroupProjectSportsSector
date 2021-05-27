bb91c233dc65f2c9f9ac2b57bb7edc99d87223bb
Dries Van de Velde <73098090+driesvdv@users.noreply.github.com>
2021-05-27 10:05:38 +0200

    websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                'host' => 'localhost',
-                'port' => 6001,
-                'scheme' => 'http',
+                'host' => 'sockets.sportplus.vandevelde.studio',
+                'port' => 443,
+                'scheme' => 'https',
             ],
         ],
 
