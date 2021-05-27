71cb2e51e29b1d68fa7da4f3646cab40c73b807f
Dries Van de Velde <73098090+driesvdv@users.noreply.github.com>
2021-05-27 09:39:45 +0200

    changes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,8 +40,7 @@ return [
                 'useTLS' => true,
                 'host' => 'localhost',
                 'port' => 6001,
-                'scheme' => 'https',
-
+                'scheme' => 'http',
             ],
         ],
 
